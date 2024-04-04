8ad8861b68b4470edc660e2cfb25c829f1ab7fdd
parthi <parthi@nanonino.in>
2024-04-04 17:09:57 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-lc';
+  loginform: FormGroup;
+  isLoading: boolean;
+  submitted: boolean;
+  hasError: boolean;
+  hide = true;
+
+
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,
+  ) { };
+
+  ngOnInit() {
+    this.initform();
+  }
+  initform() {
+    this.loginform = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email, Validators.pattern(/^\s*[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\s*$/i)])],
+      password: ['',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(100),
+        ]),
+      ],
+    })
+  }
+
+  submit() {
+    this.isLoading = true;
+    this.submitted = true;
+    const controls = this.loginform.controls;
+    if (this.loginform.invalid) {
+      Object.keys(controls).forEach(controlName => {
+        controls[controlName].markAsTouched()
+      });
+      this.isLoading = false;
+      this.toastr.error("Your Form is invalid");
+      return false;
+    } else {
+      this.toastr.success("Your Form is valid");
+      return true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserModule, ReactiveFormsModule, BrowserAnimationsModule, MatCardModule, AppRoutingModule, MatFormFieldModule,
+    MatCardModule, MatIconModule, MatButtonModule, MatCheckboxModule,  ToastrModule.forRoot({
+      timeOut: 3000
+    }) , MatToolbarModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
